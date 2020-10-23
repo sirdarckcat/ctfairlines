c25311680a59fb94cf442eb242d745d1d1a9f668
evn <evn@google.com>
2020-10-23 18:27:37 +0200

    working

--- ctf/cdls/cdls.cpp
+++ ctf/cdls/cdls.cpp
@@ -1,12 +1,18 @@
 #include "obj_dir/VCDLS.h"
 #include "verilated_vcd_c.h"
+#include "pmc825socket.h"
+#include "can_as.h"
 #include <iostream>
 #include <memory>
 #include <bitset>
 #include <valarray>
+#include <unistd.h>
+
 
 int clkcount = 0;
 int dumpit = 0;
+
+PMC825_IF Pmc825;
 
 int ascii_to_keymap(char c) {
     if (c >= 'a' && c <= 'z')
@@ -15,6 +21,7 @@ int ascii_to_keymap(char c) {
         return c - 'A' + 4;
     if (c == '{') return 2;
     if (c == '}') return 3;
+    return -1;
 }
 
 char keymap_to_ascii(int k) {
@@ -34,16 +41,16 @@ void press_button(const std::unique_ptr<VCDLS> &top, int c) {
 
 
 void reset(const std::unique_ptr<VCDLS> &top) {
-  top->reset = 0;
+  top->rst = 0;
   top->do_eval = 0;
   top->eval();
-  top->reset = 1;
+  top->rst = 1;
   top->do_eval = 0;
   top->eval();
-  top->reset = 1;
+  top->rst = 1;
   top->do_eval = 1;
   top->eval();
-  top->reset = 0;
+  top->rst = 0;
   top->do_eval = 1;
   top->eval();
   top->do_eval=0;
@@ -65,8 +72,33 @@ int main(int argc, char *argv[]) {
     auto top = std::make_unique<VCDLS>();
     // reset the door on boot.
     reset(top);
-    // listen for messages for us on the canbus
-    // press_button -> return press_button_ok
-    // get_status -> return is_locked and debug
+    // listen for messages for us on the canbus.
+    int ret = Pmc825StartInterface(&Pmc825, 0xAC1404FF, 0xAC140410, 34567, 34568, 0);
+    if (ret) {
+      // failed to start interface.
+      printf("oops\n");
+      return 1;
+    }
+    CAN_AS_MSG rx_buf, tx_buf;
+    while(1) {
+      while(1) {
+        ret = Pmc825CanAerospaceRead(&Pmc825, &rx_buf);
+        if (ret == PMC825_NO_MSG) break;
+        if (rx_buf.identifier == 1985) {
+          // 1985 = CDLS_PRESS_BUTTON
+          press_button(top, tx_buf.data[0]);
+        }
+      }
+      // 1988 = CDLS_LEDS_STATUS
+      tx_buf.identifier = 1988;
+      tx_buf.byte_count = sizeof(int);
+      tx_buf.frame_type = DATA;
+      tx_buf.node_id = 50; // 50 = CDLS_NODE_ID
+      tx_buf.data_type = AS_LONG;
+      tx_buf.data[0] = top->lights;
+      Pmc825CanAerospaceWrite(&Pmc825, &tx_buf, 1);
+      usleep(50000); // sleep for 50ms
+      top->eval();
+    }
     return 0;
 }
