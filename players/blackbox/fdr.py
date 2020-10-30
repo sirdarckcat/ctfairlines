d3ee1c4bf2e5e21cbcfa776264c65ec3990f4a98
evn <evn@google.com>
2020-10-30 23:14:23 +0100

    limits

--- players/blackbox/fdr.py
+++ players/blackbox/fdr.py
@@ -4,17 +4,31 @@ import jsonpickle
 import json
 import logging
 import logging.handlers
+import time
+import random
+import codecs
 
 lstnr = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 lstnr.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 lstnr.bind(("0.0.0.0", 34568))
 stream = pcm825.KaitaiStream(lstnr.makefile('rb'))
-handler = logging.handlers.TimedRotatingFileHandler(
-    'log/fdr-log', when='m', backupCount=3)
-logger = logging.getLogger('Flight Data Recorder')
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+
+def makeHandler(num):
+    handler = logging.handlers.TimedRotatingFileHandler(
+        'log/fdr-log-%s.log'%num, when='m', backupCount=1)
+    logger = logging.getLogger('Flight Data Recorder %s'%num)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
+
+loggers = [makeHandler(i) for i in range(20)]
 
 while True:
-    packet = pcm825.Pcm825(stream)
-    logger.info(jsonpickle.encode(packet.pcm825, unpicklable=False))
+    time.sleep(0.1)
+    for logger in loggers:
+        log = ''
+        for i in range(100):
+            packet = pcm825.Pcm825(stream)
+            log += jsonpickle.encode(packet.pcm825, unpicklable=False)
+        log = log.encode()
+        logger.info(codecs.encode(codecs.encode(log, 'zlib'), 'base64'))
--- players/blackbox/fdr.sh
+++ players/blackbox/fdr.sh
@@ -7,7 +7,7 @@ python3 fdr.py&
 cd log
 
 while true; do
-  for f in fdr-log.*; do
+  for f in fdr-log*; do
     curl -sSL -F "fdr-log=@$f" http://fdr.example.com/fdr
   done;
   sleep 10s
